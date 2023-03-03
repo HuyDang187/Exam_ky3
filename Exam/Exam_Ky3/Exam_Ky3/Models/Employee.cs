cd08bd1887a9071d823e7c6666b30d99dd02b699
HuyDang187 <101924042+HuyDang187@users.noreply.github.com>
2023-03-03 17:37:59 +0700

    Exam 3/3/2023

---
+++ Exam/Exam_Ky3/Exam_Ky3/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace Exam_Ky3.Models
+{
+	public class Employee
+	{
+		public long ID { get; set; }
+		public string? Name { get; set; }
+		public bool Status { get; set; }
+	}
+}
---
+++ Exam/Exam_Ky3/Exam_Ky3/Models/TodoContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Exam_Ky3.Models
+{
+	public class TodoContext : DbContext
+	{
+		public TodoContext(DbContextOptions<TodoContext> options)
+			: base(options)
+		{
+		}
+
+		public DbSet<Employee> Employees { get; set; } = null!;
+	}
+}
